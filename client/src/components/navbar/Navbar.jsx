56c9946e2d4e4521877d443550ef8ec6eb07f36a
AYUSH <ayushrajbls@gmail.com>
2023-10-15 09:52:36 +0530

    initial commit

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>
+
+<div className="p-8">
+<ul className="flex gap-10">
+    <li>Logo</li>
+    <li>Solutions</li>
+    <li>Features</li>
+    <li>Blogs</li>
+    <li>About</li>
+    
+</ul>
+</div>
+<div>
+    <button>Login</button>
+    <button>Register</button>
+</div>
+
+  </div>;
+};
+
+export default Navbar;
